3d3f4872ae343cdff8d2412825af7bcc2d1bee22
upforge-in <lucky.tiwari.png@gmail.com>
2026-02-20 17:51:48 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,15 @@
-// tailwind.config.ts
-plugins: [
-  require("tailwindcss-animate"),
-  require("tw-animate-css")
-],
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("tailwindcss-animate")],
+};
+
+export default config;
