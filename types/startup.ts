ff0e817a57f12308fcb9fdc888401201953303c9
upforge-in <lucky.tiwari.png@gmail.com>
2026-02-21 17:41:41 +0530

    Update startup.ts

--- types/startup.ts
+++ types/startup.ts
@@ -4,35 +4,9 @@ export interface Startup {
   slug: string
   logo_url: string
   description: string
+  short_description?: string // Added to support the directory view
   website: string
-// upforge-in/upforge/Upforge-b69600313d9aaf587d9db2d25aef7d68eebcbd33/types/startup.ts
 
-export interface Startup {
-  id: string
-  name: string
-  slug: string
-  logo_url: string
-  description: string
-  short_description?: string // Added here
-  website: string
-  founders: string | string[]
-  founded_year: number
-  category?: string
-  location?: string
-  is_sponsored?: boolean
-  is_featured?: boolean
-  created_at?: string
-}
-
-export interface StartupDirectoryItem {
-  id: string
-  name: string
-  slug: string
-  logo_url: string
-  short_description?: string // Added here to fix the build error
-  is_sponsored?: boolean
-  is_featured?: boolean
-}
   // allow both single string and array
   founders: string | string[]
 
@@ -49,6 +23,7 @@ export interface StartupDirectoryItem {
   name: string
   slug: string
   logo_url: string
+  short_description?: string // Added to fix the "Property does not exist" error
   is_sponsored?: boolean
   is_featured?: boolean
 }
